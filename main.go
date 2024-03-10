d7aaaaa9798a3334f6a3c081c75e18e54557e4a5
AlexandrPirogov <devalexandrpirogov@gmail.com>
2024-03-10 12:27:23 -0400

    fix

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Jenkins! This is a simple Go web application.")
+	fmt.Fprintf(w, "Hello, Jenkins! This is a simple Go web application.\n")
 	fmt.Fprintf(w, "Added new line with CI/CD")
 }
 
